ac733df4eb1b215dc309bfed516668f6b8edc479
ankoosh544 <63229690+ankoosh544@users.noreply.github.com>
2023-07-28 18:00:07 +0200

    code modified in device_connected class

--- sofia_app/lib/screens/home/device_connected.dart
+++ sofia_app/lib/screens/home/device_connected.dart
@@ -7,10 +7,13 @@ import 'package:sofia_app/configs/index.dart';
 import 'package:sofia_app/providers/index.dart';
 
 class DeviceConnected extends StatelessWidget {
-  const DeviceConnected({super.key});
+  const DeviceConnected({Key? key});
 
   @override
   Widget build(BuildContext context) {
+    final bleProvider = context.read<BleProvider>();
+    final scanResults = bleProvider.scanResult;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(tabHome),
@@ -19,69 +22,45 @@ class DeviceConnected extends StatelessWidget {
         shadowColor: Theme.of(context).shadowColor,
         elevation: 1,
       ),
-      body: Column(
-        children: [
-          Text(
-            'Welcome User {}',
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-          StreamBuilder<List<ScanResult>>(
-            stream: context.read<BleProvider>().scanResult,
-            initialData: const [],
-            builder: (c, snapshot) => Column(
-              children: (snapshot.data ?? [])
-                  .map(
-                    (r) => Text(
-                      '${r.device.localName}: ${r.rssi.toString()}',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  )
-                  .toList(),
-            ),
-          ),
-          // StreamBuilder<List<BluetoothDevice>>(
-          //   stream: context.read<BleProvider>().connectedDeviceStream,
-          //   initialData: const [],
-          //   builder: (c, snapshot) {
-          //     return Column(
-          //       children: snapshot.data!.map((device) {
-          //         log('Connected Device : ${device.toString()}');
-          //         return Column(
-          //           children: [
-          //             ListTile(
-          //               title: Text(device.localName),
-          //               subtitle: Text(device.remoteId.toString()),
-          //               trailing: StreamBuilder<BluetoothConnectionState>(
-          //                 stream: device.connectionState,
-          //                 initialData: BluetoothConnectionState.disconnected,
-          //                 builder: (c, snapshot) {
-          //                   if (snapshot.data ==
-          //                       BluetoothConnectionState.connected) {
-          //                     return StreamBuilder<int>(
-          //                         stream: context
-          //                             .read<BleProvider>()
-          //                             .rssiStream(device),
-          //                         builder: (context, snapshot) {
-          //                           return Text(
-          //                               snapshot.hasData
-          //                                   ? '${snapshot.data}dBm'
-          //                                   : '',
-          //                               style: Theme.of(context)
-          //                                   .textTheme
-          //                                   .bodySmall);
-          //                         });
-          //                   }
-          //                   return Text(snapshot.data.toString());
-          //                 },
-          //               ),
-          //             ),
-          //           ],
-          //         );
-          //       }).toList(),
-          //     );
-          //   },
-          // ),
-        ],
+      body: StreamBuilder<List<ScanResult>>(
+        stream: scanResults,
+        initialData: const [],
+        builder: (c, snapshot) {
+          final scanResults = snapshot.data ?? [];
+          print("ScanResult******************");
+          print(scanResults);
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+
+            
+            children: [
+              if (scanResults.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    'Welcome User!', // Replace 'User' with the actual user name or data
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                ),
+              Expanded(
+                child: scanResults.isEmpty
+                    ? Center(
+                        child: Text('No devices are connected'),
+                      )
+                    : ListView.builder(
+                        itemCount: scanResults.length,
+                        itemBuilder: (context, index) {
+                          final r = scanResults[index];
+                          return Text(
+                            '${r.device.localName}: ${r.rssi.toString()}',
+                            style: TextStyle(fontSize: 20),
+                          );
+                        },
+                      ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
